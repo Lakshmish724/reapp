8235cf19fe2361169d1128a481fa7a76136feb37
Lakshmish <Lakshmish504@gmail.com>
2021-04-29 06:35:25 -0500

    five

---
+++ reaapp/src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+
+
+export class Home extends Component{
+
+    render(){
+        return(
+            <div className="m-3 d-flex justify-content-left">      
+                <div class="image">
+                   <img src="https://iso.500px.com/wp-content/uploads/2015/12/stock-photo-125301449.jpg"/>
+                </div>
+            </div>
+
+        )
+    }
+}
